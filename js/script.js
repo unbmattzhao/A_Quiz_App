a60e32ca640a58c6744ea89538d10cbe696204ff
Matt Zhao <46049501+JediRebel@users.noreply.github.com>
2023-02-07 19:52:57 -0400

    more js code
    add functions to dispaly the questions and answers, tried to loop from the first question but failed, need more work

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ const questionArray =
     },
 
     {
-        Question: "String valuse must be enclosed within ____ when being assigned to variables.",
+        Question: "String values must be enclosed within ____ when being assigned to variables.",
         option: [
                 "1. commas",    
                 "2. curly brackets",
@@ -52,23 +52,32 @@ const questionArray =
                 "1. JavaScript",    
                 "2. terminal / bash",
                 "3. for loops",    
-                "4. cosole.log"
+                "4. console.log"
                 ],
-        answer:   "4. cosole.log"
+        answer:   "4. console.log"
     }
 ]
 
 // get all the related elements
+var viewScore = document.querySelector('.viewScore');
+var timer = document.querySelector('#timer');
 var firstPage = document.querySelector('.firstPage');
 var startBtn = document.querySelector('#startBtn');
 var qaArea = document.querySelector('#qaArea');
-var question = document.querySelector('.question');
+var qaResultEL = document.querySelector('#qaResult');
+var hrEl = document.querySelector('.hr');
+var correctEl = document.querySelector('.correct');
+var incorrectEl = document.querySelector('.incorrect')
+var questionEl = document.querySelector('.question');
+var answerBtnEl = document.querySelector('.answerBtn');
 var scoreSubmissionArea = document.querySelector('#scoreSubmissionArea');
 var goBackArea = document.querySelector('.goBackArea');
-var choice1 = document.querySelector('#choice1');
-var choice2 = document.querySelector('#choice2');
-var choice3 = document.querySelector('#choice3');
-var choice4 = document.querySelector('#choice4');
+var optionBtn0 = document.querySelector('#optionBtn0');
+var optionBtn1 = document.querySelector('#optionBtn1');
+var optionBtn2 = document.querySelector('#optionBtn2');
+var optionBtn3 = document.querySelector('#optionBtn3');
+var i = 0;
+var score = 0;
 
 
 
@@ -77,19 +86,88 @@ qaArea.setAttribute('style', 'display: none');
 scoreSubmissionArea.setAttribute('style', 'display: none');
 goBackArea.setAttribute('style', 'display: none');
 
-// Show the first question when clicking the Start Quiz button
+// Hide the first page, show the questions from the first one when clicking the Start Quiz button
 
 startBtn.addEventListener('click', () => {
     firstPage.setAttribute('style', 'display: none');
-    qaArea.removeAttribute('style', 'display: none')
+    qaArea.removeAttribute('style');
+    hrEl.setAttribute('style', 'display: none');
+    correctEl.setAttribute('style', 'display: none');
+    incorrectEl.setAttribute('style', 'display: none');
+    timer.innerText = 'Time: 75';
+
+    setTime();
+
 }
 );
 
-//! find out how to get value from an object inside an array
-question.innerText = (
-    () => {
-        for (let i = 0; i < questionArray.length; i++){
-            return questionArray[i][i];
-        }
+function showNextQuestion() {  
+    if(i<questionArray.length) {
+        i++;
+        showQuestion();
+    }
+    else{
+        ""
+    };
+}
+function showQuestion() {
+    for(var i=0; i<questionArray.length; i++){
+    questionEl.innerText = questionArray[i].Question;
+    optionBtn0.innerText = questionArray[i].option[0];
+    optionBtn1.innerText = questionArray[i].option[1];
+    optionBtn2.innerText = questionArray[i].option[2];
+    optionBtn3.innerText = questionArray[i].option[3];
+}  
+};
 
-});
+showQuestion();
+
+answerBtnEl.addEventListener('click', showNextQuestion);
+
+function backToQuiz(){
+    location.reload();
+}
+
+function calcScore(e){
+    if(e.innerHTML===questionArray[i].answer && score<questionArray.length)
+    {score= score+1}
+    else{}
+    setTimeout(showNextQuestion(),300);
+}
+
+
+
+
+// i ++;
+
+// showNextQuestion();
+
+
+// question.innerText = (
+//     function question() {
+//         for (let i = 0; i < questionArray.length; i++){
+//             questionArray.question;
+//         }
+// console.log(questionArray.question);
+
+// });
+
+// set timer 75' 
+
+var timeLeft = 75;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    timeLeft--;
+    timer.textContent = "Time: " + timeLeft;
+
+    if(timeLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // return result
+      result();
+    }
+
+  }, 1000);
+}
